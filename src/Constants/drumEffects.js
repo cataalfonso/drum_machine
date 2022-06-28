e8531a6798aeaa88c37fc73763a347237668dee6
Cata Alfonso <cataalfonso@Catas-MacBook-Air.local>
2022-06-29 01:08:32 +0200

    final changes

---
+++ src/Constants/drumEffects.js
@@ -0,0 +1,14 @@
+
+const drumEffects = [
+    { keyboard: "Q", audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', describeAudio: "Bass Boom", keyNumber: 81 },
+    { keyboard: "W", audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', describeAudio: "Drum pulse", keyNumber: 87 },
+    { keyboard: "E", audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3', describeAudio: "Drum fills", keyNumber: 69 },
+    { keyboard: "A", audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', describeAudio: "Fillin", keyNumber: 65 },
+    { keyboard: "S", audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', describeAudio: "Failure drum", keyNumber: 83 },
+    { keyboard: "D", audio: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3', describeAudio: "Golpe de aro", keyNumber: 68 },
+    { keyboard: "Z", audio: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3', describeAudio: "Cymbal and bass", keyNumber: 90 },
+    { keyboard: "X", audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', describeAudio: "Bass", keyNumber: 88 },
+    { keyboard: "C", audio: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', describeAudio: "Punch Boxing", keyNumber: 67 },
+  ];
+
+  export default drumEffects
